3ce0db719819baeacf4c77c514fb754f49059886
Leo Hrvat <leo.hrvat@stud.hslu.ch>
2025-05-01 10:15:58 +0200

    Themenfokus
    Added RoutineExample and FunctionalExample

---
+++ FunctionalPatterns/FunctionalExamples.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Apply(f func(int) int, val int) int {
+	return f(val)
+}
+
+func double(val int) int {
+	return val * 2
+}
+
+func triple(val int) int {
+	return val * 3
+}
+
+func square(val int) int {
+	return val * val
+}
+
+func main() {
+	fmt.Println(Apply(double, 4))
+	fmt.Println(Apply(triple, 5))
+	fmt.Print(Apply(square, 4))
+}
